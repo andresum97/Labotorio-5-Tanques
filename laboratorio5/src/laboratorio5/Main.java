52682991157bd0c126f551fb5cc3f2ac20a517e3
sam17332 <32148906+sam17332@users.noreply.github.com>
2017-11-05 17:46:08 -0600

    cambios al main

--- laboratorio5/src/laboratorio5/Main.java
+++ laboratorio5/src/laboratorio5/Main.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -19,6 +20,10 @@ public class Main {
 	private JTextField base;
 	private JTextField largo;
 	private JTextField cantidadAgua;
+	private JLabel lblbase;
+	private JLabel lblAl;
+	private JLabel lblLargo;
+	private JComboBox tanques;
 
 	/**
 	 * Launch the application.
@@ -58,6 +63,16 @@ public class Main {
 		frame.getContentPane().setLayout(null);
 		
 		tipos = new JComboBox();
+		tipos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(tipos.getSelectedIndex() == 1) {
+					lblbase.setText("Radio");
+					lblLargo.setVisible(false);
+					largo.setVisible(false);
+				}	
+				
+			}
+		});
 		tipos.setModel(new DefaultComboBoxModel(new String[] {"Tipos de Tanque:", "Cilindrico", "Cubico", "Otogonal"}));
 		tipos.setBounds(17, 21, 164, 27);
 		frame.getContentPane().add(tipos);
@@ -66,15 +81,15 @@ public class Main {
 		lblDimensionales.setBounds(223, 25, 114, 16);
 		frame.getContentPane().add(lblDimensionales);
 		
-		JLabel lblAl = new JLabel("Altura:");
+		lblAl = new JLabel("Altura:");
 		lblAl.setBounds(223, 48, 61, 16);
 		frame.getContentPane().add(lblAl);
 		
-		JLabel lblBase = new JLabel("Ancho:");
-		lblBase.setBounds(223, 73, 61, 16);
-		frame.getContentPane().add(lblBase);
+		lblbase = new JLabel("Ancho:");
+		lblbase.setBounds(223, 73, 61, 16);
+		frame.getContentPane().add(lblbase);
 		
-		JLabel lblLargo = new JLabel("Largo:");
+		lblLargo = new JLabel("Largo:");
 		lblLargo.setBounds(223, 98, 61, 16);
 		frame.getContentPane().add(lblLargo);
 		
@@ -98,11 +113,11 @@ public class Main {
 		frame.getContentPane().add(cantidadAgua);
 		cantidadAgua.setColumns(10);
 		
-		JLabel lblCantidadDeAgua = new JLabel("Cantidad de agua:");
-		lblCantidadDeAgua.setBounds(223, 132, 114, 16);
+		JLabel lblCantidadDeAgua = new JLabel("Cantidad de agua (Litros):");
+		lblCantidadDeAgua.setBounds(223, 132, 164, 16);
 		frame.getContentPane().add(lblCantidadDeAgua);
 		
-		JComboBox tanques = new JComboBox();
+		tanques = new JComboBox();
 		tanques.setModel(new DefaultComboBoxModel(new String[] {"Numero de tanque:", ""}));
 		tanques.setBounds(493, 21, 176, 27);
 		frame.getContentPane().add(tanques);
@@ -118,10 +133,40 @@ public class Main {
 		JButton btnAgregarTanque = new JButton("Agregar tanque");
 		btnAgregarTanque.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				altura.setText("");
-				base.setText("");
-				largo.setText("");
-				cantidadAgua.setText("");
+
+				lblLargo.setVisible(true);
+				largo.setVisible(true);
+				if(tipos.getSelectedIndex() == 0)
+				{
+					JOptionPane.showMessageDialog(null, "Escoja un tipo de tanque.");
+				}
+				if(tipos.getSelectedIndex() == 1)
+				{
+					tanques.insertItemAt(altura.getText() +"x"+ base.getText() +" cantidad de agua: "+ cantidadAgua.getText() +"L", 1);
+					altura.setText("");
+					base.setText("");
+					cantidadAgua.setText("");
+					lblbase.setText("Base");
+					tipos.setSelectedIndex(0);
+				}
+				if(tipos.getSelectedIndex() == 2)
+				{
+					tanques.insertItemAt(altura.getText() +"x"+ base.getText() +"x"+ largo.getText() +" cantidad de agua: "+ cantidadAgua.getText() +"L",1);
+					tipos.setSelectedIndex(0);
+					altura.setText("");
+					base.setText("");
+					largo.setText("");
+					cantidadAgua.setText("");
+				}
+				if(tipos.getSelectedIndex() == 3)
+				{
+					tanques.insertItemAt(altura.getText() +"x"+ base.getText() +"x"+ largo.getText() +" cantidad de agua: "+ cantidadAgua.getText() +"L",1);
+					tipos.setSelectedIndex(0);
+					altura.setText("");
+					base.setText("");
+					largo.setText("");
+					cantidadAgua.setText("");
+				}
 			}
 		});
 		btnAgregarTanque.setBounds(223, 188, 140, 29);
@@ -137,10 +182,10 @@ public class Main {
 		btnSalir.setBounds(301, 259, 117, 29);
 		frame.getContentPane().add(btnSalir);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Disponibilidad", "Abierto", "Cerrado"}));
-		comboBox.setBounds(493, 94, 176, 27);
-		frame.getContentPane().add(comboBox);
+		JComboBox disponibilidad = new JComboBox();
+		disponibilidad.setModel(new DefaultComboBoxModel(new String[] {"Disponibilidad", "Abierto", "Cerrado"}));
+		disponibilidad.setBounds(493, 94, 176, 27);
+		frame.getContentPane().add(disponibilidad);
 		
 		JButton btnNivelDeAgua = new JButton("Nivel de Agua");
 		btnNivelDeAgua.setBounds(17, 259, 117, 29);
